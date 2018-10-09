88074ee40a16a96c73424e8b369f292d712ea4a2
TRBruines <tim.bruines@student.hu.nl>
2018-10-09 10:07:05 +0200

    update

--- code5.py
+++
@@ -1,8 +0,0 @@
-grondgetallen=[ 4, 5, 3, -81 ]
-def kwadraten_som(grondgetallen):
-    sum = 0
-    for c in grondgetallen:
-        if c > 0:
-            sum += c**2
-    print(sum)
-kwadraten_som(grondgetallen)
